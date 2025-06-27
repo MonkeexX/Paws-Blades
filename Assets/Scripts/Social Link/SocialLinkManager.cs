a5cc3fd2b44d5927a7d28a6bb9a55d93a80c1a97
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-06-27 16:58:51 +0200

    Converted SocialLinkManager to Singleton

--- Assets/Scripts/Social Link/SocialLinkManager.cs
+++ Assets/Scripts/Social Link/SocialLinkManager.cs
@@ -5,11 +5,17 @@ namespace SocialLink
 {
     public class SocialLinkManager : MonoBehaviour
     {
+        public static SocialLinkManager Instance { get; private set; }
+
         public CharacterFileContainer characterFileContainer;
         public Dictionary<string, CharacterFile> files = new Dictionary<string, CharacterFile>();
 
         private void Start()
         {
+            if (Instance != null)
+                Destroy(this.gameObject);
+            DontDestroyOnLoad(this.gameObject);
+
             foreach (CharacterFile file in characterFileContainer.CharacterFiles)
             {
                 files.Add(file.Name, new CharacterFile(file));
